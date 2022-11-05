f5fbbbd5c48ee034711ef35f3a434126fa64676e
BeratDamar <berat_damar7@hotmail.com>
2022-11-05 20:43:34 +0300

    homepage is implemented with welcome messege

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PostBox from '../../components/PostBox/PostBox';
 import './HomePage.css';
 const HomePage = () => {
-  return (
-    <div className='home-container'>
+  return  (
+    <div className='general-login-container'>
+      <div className='home-container'>
       <div className='welcome-text'>Welcome to smartive</div>
-      // <PostBox />
-
-      <h1>Homepage</h1>
+      
+    </div>
     </div>
   );
 };
